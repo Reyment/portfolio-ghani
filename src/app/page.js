ae62ebeb9686379f52603e39c630e479bae6073d
mnyabdulghani <mnyabdulghani@gmail.com>
2023-10-23 19:16:24 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Hello world</h1>
+    <main className='flex min-h-screen flex-col bg-[#121212]'>
+      <Navbar />
+      <div className='container mt-24 mx-auto px-12 py-4'>
+        <HeroSection />
+        
       </div>
+
+
     </main>
 
   )
