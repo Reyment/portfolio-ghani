b2083e7e4ef53f1108be09b39a6c472644dd42c2
mnyabdulghani <mnyabdulghani@gmail.com>
2023-10-24 12:12:13 +0700

    lagi

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import AboutSection from './components/AboutSection'
 import AchievementsSection from './components/AchievementsSection'
 import EmailSection from './components/EmailSection'
 import Footer from './components/Footer'
-import HeroSection from './components/HeroSection'
+// import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 import ProjectsSection from './components/ProjectsSection'
 
@@ -11,7 +11,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col bg-[#121212]'>
       <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
-        <HeroSection />
+        {/* <HeroSection /> */}
         <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
