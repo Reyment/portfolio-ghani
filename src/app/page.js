f7ada52553eb13b416ee780dd5ce88a90673d3bc
mnyabdulghani <mnyabdulghani@gmail.com>
2023-10-24 12:19:51 +0700

    testingggg

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import AboutSection from './components/AboutSection'
 import AchievementsSection from './components/AchievementsSection'
 import EmailSection from './components/EmailSection'
 import Footer from './components/Footer'
-// import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 import ProjectsSection from './components/ProjectsSection'
 
@@ -11,7 +10,6 @@ export default function Home() {
     <main className='flex min-h-screen flex-col bg-[#121212]'>
       <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
-        {/* <HeroSection /> */}
         <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
