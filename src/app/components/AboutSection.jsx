52783aa5af5f4e2f724660bb674b9293b7da40af
mnyabdulghani <mnyabdulghani@gmail.com>
2023-10-24 12:02:05 +0700

    terbaru

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>PHP</li>
+        <li>Sequelize</li>
+        <li>Angular</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>SMKN 8 MALANG - software engineering</li>
+        <li>SMPN 14 Malang</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>IT Support Google</li>
+        <li>Oracle Academy - Java Foundation</li>
+        <li>Oracle Academy - Java Fundamentals</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" alt="computer" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I am a programmer with a dedication to producing innovative and efficient software solutions. With 3 years of experience in software development, I have a deep understanding of the development cycle and the ability to translate concepts into functional code. With my educational background in IT I have built a strong foundation in software engineering principles.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import AboutSection from './components/AboutSection'
+import AchievementsSection from './components/AchievementsSection'
+import EmailSection from './components/EmailSection'
+import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
+import ProjectsSection from './components/ProjectsSection'
 
 export default function Home() {
   return (
@@ -7,8 +12,12 @@ export default function Home() {
       <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
         <HeroSection />
-        
+        <AchievementsSection />
+        <AboutSection />
+        <ProjectsSection />
+        <EmailSection />
       </div>
+      <Footer />
 
 
     </main>
