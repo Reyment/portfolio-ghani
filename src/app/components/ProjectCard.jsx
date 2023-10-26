663237adbf25e28e9ebc300950bb36b032e987cf
mnyabdulghani <mnyabdulghani@gmail.com>
2023-10-26 19:47:30 +0700

    fix3

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
       <div
-        className="h-72 w-10 md:w-full md:h-80 rounded-t-xl relative group"
+        className="h-72 w-52 md:w-full md:h-80 rounded-t-xl relative group"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
