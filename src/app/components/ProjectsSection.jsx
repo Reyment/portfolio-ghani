fcde265b5f90896a1d1368435ff5e2ded973a0eb
Muhammad Nur Yaqut Abdul Ghani <93895499+Reyment@users.noreply.github.com>
2024-05-28 07:12:03 +0700

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -28,57 +28,65 @@ const projectsData = [
     description: "",
     image: "/images/projects/portofolio13.png",
     tag: ["Web"],
-    previewUrl: "https://penuhmakna.com/",
+    previewUrl: "https://penuh-makna.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Real Estate Website",
+    description: "",
+    image: "/images/projects/portofolio15.png",
+    tag: ["Web"],
+    previewUrl: "https://example-convert-figma-html-3.vercel.app/",
   },
 
-  {
-    id: 7,
-    title: "Ilustrator Design",
-    description: "",
-    image: "/images/projects/portofolio7.png",
-    tag: ["Design"],
-    previewUrl: "/",
-  },
-  {
-    id: 8,
-    title: "Ilustrator Design",
-    description: "",
-    image: "/images/projects/portofolio8.png",
-    tag: [ "Design"],
-    previewUrl: "/",
-  },
-  {
-    id: 9,
-    title: "Flyer Design",
-    description: "",
-    image: "/images/projects/portofolio9.png",
-    tag: [ "Design"],
-    previewUrl: "/",
-  },
-  {
-    id: 10,
-    title: "Flyer Design",
-    description: "",
-    image: "/images/projects/portofolio10.png",
-    tag: [ "Design"],
-    previewUrl: "/",
-  },
-  {
-    id: 11,
-    title: "Mock Up Desin",
-    description: "",
-    image: "/images/projects/portofolio11.png",
-    tag: ["Design"],
-    previewUrl: "/",
-  },
-  {
-    id: 12,
-    title: "Logo Design",
-    description: "",
-    image: "/images/projects/portofolio12.png",
-    tag: ["Design"],
-    previewUrl: "/",
-  },
+  // {
+  //   id: 7,
+  //   title: "Ilustrator Design",
+  //   description: "",
+  //   image: "/images/projects/portofolio7.png",
+  //   tag: ["Design"],
+  //   previewUrl: "/",
+  // },
+  // {
+  //   id: 8,
+  //   title: "Ilustrator Design",
+  //   description: "",
+  //   image: "/images/projects/portofolio8.png",
+  //   tag: [ "Design"],
+  //   previewUrl: "/",
+  // },
+  // {
+  //   id: 9,
+  //   title: "Flyer Design",
+  //   description: "",
+  //   image: "/images/projects/portofolio9.png",
+  //   tag: [ "Design"],
+  //   previewUrl: "/",
+  // },
+  // {
+  //   id: 10,
+  //   title: "Flyer Design",
+  //   description: "",
+  //   image: "/images/projects/portofolio10.png",
+  //   tag: [ "Design"],
+  //   previewUrl: "/",
+  // },
+  // {
+  //   id: 11,
+  //   title: "Mock Up Desin",
+  //   description: "",
+  //   image: "/images/projects/portofolio11.png",
+  //   tag: ["Design"],
+  //   previewUrl: "/",
+  // },
+  // {
+  //   id: 12,
+  //   title: "Logo Design",
+  //   description: "",
+  //   image: "/images/projects/portofolio12.png",
+  //   tag: ["Design"],
+  //   previewUrl: "/",
+  // },
   
 ];
 
@@ -112,11 +120,11 @@ const ProjectsSection = () => {
           name="Web"
           isSelected={tag === "Web"}
         />
-        <ProjectTag
+        {/* <ProjectTag
           onClick={handleTagChange}
           name="Design"
           isSelected={tag === "Design"}
-        />
+        /> */}
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
